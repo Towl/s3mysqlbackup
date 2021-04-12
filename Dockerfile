3be2004e5057825244d97f8229f05d952298f4d3
Franck <franck.coutouly@tms-software.ch>
2021-04-12 16:49:27 +0200

    Add restore.sh

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ RUN apk add mysql-client pv coreutils
 COPY backup.sh /tmp
 COPY restore.sh /tmp
 
-RUN chmod a+x /tmp/backup.sh
+RUN chmod a+x /tmp/backup.sh /tmp/restore.sh
 
 WORKDIR /tmp
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 VERSION=$1
 
 docker pull google/cloud-sdk:alpine
-docker build -t eu.gcr.io/warm-canto-276410/s3filebackup:latest .
-docker build -t eu.gcr.io/warm-canto-276410/s3filebackup:$VERSION .
-docker push eu.gcr.io/warm-canto-276410/s3filebackup:latest
-docker push eu.gcr.io/warm-canto-276410/s3filebackup:$VERSION
+docker build -t eu.gcr.io/warm-canto-276410/s3-mysql-backup:latest .
+docker build -t eu.gcr.io/warm-canto-276410/s3-mysql-backup:$VERSION .
+docker push eu.gcr.io/warm-canto-276410/s3-mysql-backup:latest
+docker push eu.gcr.io/warm-canto-276410/s3-mysql-backup:$VERSION
